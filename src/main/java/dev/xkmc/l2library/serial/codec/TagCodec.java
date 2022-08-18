3b548e724d3d2cfedfea9e58d142a7291a884694
lcy0x1 <arthurlcy0x1@gmail.com>
2022-08-18 15:21:49 +0800

    add value TagCodec, bump to 1.7.3

--- src/main/java/dev/xkmc/l2library/serial/codec/TagCodec.java
+++ src/main/java/dev/xkmc/l2library/serial/codec/TagCodec.java
@@ -4,8 +4,10 @@ import dev.xkmc.l2library.serial.SerialClass;
 import dev.xkmc.l2library.serial.unified.TagContext;
 import dev.xkmc.l2library.serial.unified.UnifiedCodec;
 import dev.xkmc.l2library.serial.wrapper.ClassCache;
+import dev.xkmc.l2library.serial.wrapper.TypeInfo;
 import dev.xkmc.l2library.util.code.Wrappers;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 
 import javax.annotation.Nullable;
 import java.util.function.Predicate;
@@ -37,5 +39,27 @@ public class TagCodec {
 	public static CompoundTag toTag(CompoundTag tag, Class<?> cls, Object obj, Predicate<SerialClass.SerialField> pred) {
 		return Wrappers.get(() -> UnifiedCodec.serializeObject(new TagContext(pred), tag, ClassCache.get(cls), obj));
 	}
+
+	@Nullable
+	@SuppressWarnings("unchecked")
+	public static <T> T valueFromTag(Tag tag, Class<?> cls, Predicate<SerialClass.SerialField> pred) {
+		return (T) Wrappers.get(() -> UnifiedCodec.deserializeValue(new TagContext(pred), tag, TypeInfo.of(cls), null));
+	}
+
+	@Nullable
+	public static Tag valueToTag(Class<?> cls, Object obj, Predicate<SerialClass.SerialField> pred) {
+		return Wrappers.get(() -> UnifiedCodec.serializeValue(new TagContext(pred), TypeInfo.of(cls), obj));
+	}
+
+	@Nullable
+	public static <T> T valueFromTag(Tag tag, Class<?> cls) {
+		return valueFromTag(tag, cls, e -> true);
+	}
+
+	@Nullable
+	public static Tag valueToTag(Object obj) {
+		return valueToTag(obj.getClass(), obj, e -> true);
+	}
+
 
 }
