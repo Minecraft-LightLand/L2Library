7a850cc9413d6d3f2344873ddc00ab1df99723a1
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-08-03 13:43:27 +0800

    release

--- src/main/java/dev/xkmc/l2library/events/MiscServerEventHandler.java
+++ src/main/java/dev/xkmc/l2library/events/MiscServerEventHandler.java
@@ -1,10 +1,13 @@
 package dev.xkmc.l2library.events;
 
 import dev.xkmc.l2library.content.explosion.BaseExplosion;
-import dev.xkmc.l2library.init.L2Library;
 import dev.xkmc.l2library.content.raytrace.RayTraceUtil;
+import dev.xkmc.l2library.init.FlagMarker;
+import dev.xkmc.l2library.init.L2Library;
+import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.EntityStruckByLightningEvent;
 import net.neoforged.neoforge.event.level.ExplosionEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
@@ -20,6 +23,15 @@ public class MiscServerEventHandler {
 	public static void onDetonate(ExplosionEvent.Detonate event) {
 		if (event.getExplosion() instanceof BaseExplosion exp) {
 			event.getAffectedEntities().removeIf(e -> !exp.hurtEntity(e));
+		}
+	}
+
+	@SubscribeEvent
+	public static void onEntityStruck(EntityStruckByLightningEvent event) {
+		if (event.getLightning().getTags().contains(FlagMarker.LIGHTNING)) {
+			if (!(event.getEntity() instanceof LivingEntity) || event.getEntity() == event.getLightning().getCause()) {
+				event.setCanceled(true);
+			}
 		}
 	}
 
