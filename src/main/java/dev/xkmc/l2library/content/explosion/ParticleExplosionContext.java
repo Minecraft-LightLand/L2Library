076d6cafd7d7cc4a0968597f4dbbc44b791d29d2
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-09-12 17:15:49 +0800

    lib update

--- src/main/java/dev/xkmc/l2library/content/explosion/ParticleExplosionContext.java
+++ src/main/java/dev/xkmc/l2library/content/explosion/ParticleExplosionContext.java
@@ -2,9 +2,23 @@ package dev.xkmc.l2library.content.explosion;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 
 public record ParticleExplosionContext(ParticleOptions small,
 									   ParticleOptions large,
 									   Holder<SoundEvent> sound) {
+
+	public static final ParticleExplosionContext INS = new ParticleExplosionContext(
+			ParticleTypes.EXPLOSION, ParticleTypes.EXPLOSION_EMITTER, SoundEvents.GENERIC_EXPLODE
+	);
+
+	public static ParticleExplosionContext of(float radius) {
+		return new ParticleExplosionContext(
+				radius < 2 ? ParticleTypes.EXPLOSION : ParticleTypes.EXPLOSION_EMITTER,
+				ParticleTypes.EXPLOSION_EMITTER, SoundEvents.GENERIC_EXPLODE
+		);
+	}
+
 }
